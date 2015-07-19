c04250bd3e25ad6d78b8e047cb03a786ae4c0356
Kabiraa <arpitlokwani@Arpits-MacBook-Pro.local>
2015-07-19 15:54:20 +0530

    Initial Commit

---
+++ CustomTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTableView
+//
+//  Created by Arpit Lokwani on 7/19/15.
+//  Copyright (c) 2015 Arpit Lokwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomTableView
+//
+//  Created by Arpit Lokwani on 7/19/15.
+//  Copyright (c) 2015 Arpit Lokwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
